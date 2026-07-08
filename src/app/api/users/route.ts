14be27de78036a7b9ad67f411121e826676417a6
Udayanga Damunugama <udayanga.damunugama@example.com>
2026-07-08 13:01:40 +0530

    fix: disable caching for users api

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+export const dynamic = 'force-dynamic';
 import { adminAuth, adminInitError } from "../../../lib/firebaseAdmin";
 
 export async function GET() {
